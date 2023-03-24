b2fd939c8d47f0a99d87203a4a7fbdd6f7b2390e
Gabriel Jordão Alonso <gabrieljordaoalonso@gmail.com>
2023-03-24 11:04:26 +0000

    add logo and create css login

--- index.php
+++ index.php
@@ -1,12 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - Online Shopping</title>
+    <link href="css/bootstrap-reboot.min.css" rel="stylesheet" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/login.css" rel="stylesheet" />
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
 </head>
-<body>
-    
+
+<body class="text-center">
+    <main class="form-signin">
+        <form>
+            <img class="mb-4" src="img/logo.png" alt="" width="229" height="148">
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+            <p class="bg-danger text-white p-2 fw-bold">Please use this user!<br />Login: user | Password: pass123</p>
+
+            <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" autocomplete="off">
+                <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" autocomplete="off">
+                <label for="floatingPassword">Password</label>
+            </div>
+
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+            <p class="mt-5 mb-3 text-muted">Gabriel J. Alonso - 2023</p>
+
+            
+
+        </form>
+    </main>
+
+    <script src="../js/bootstrap/bootstrap.bundle.min.js"></script>
+    <script src="js/script.js"></script>
 </body>
+
 </html>
